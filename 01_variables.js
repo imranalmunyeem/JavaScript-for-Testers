a271f3c20cf401a8480bddf6d203fce6d7a0f046
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2022-05-15 00:17:10 +0600

    Variables

---
+++ 01_variables.js
@@ -0,0 +1,30 @@
+//---------------------------------Let------------------------------------------//
+let name = 'Imran';
+console.log('Name is:' + name);
+
+let age = 26;
+console.log('Age is:' + age);
+
+let ageMoreThan18 = true;  //Boolean
+console.log('Age is more than 18:' + ageMoreThan18);
+
+//-----------------------------Constant------------------------------------------//
+const name1 = 'Imran';
+console.log('Name is:' + name1);
+
+const age1 = 26;
+console.log('Age1 is:' + age1);
+
+const ageMoreThan20 = true;  //Boolean
+console.log('Age1 is more than 20:' + ageMoreThan20);
+
+//---------------------------------Var------------------------------------------//
+var name2 = 'Imran';
+console.log('Name is:' + name2);
+
+var age2 = 26;
+console.log('Age2 is:' + age2);
+
+var ageMoreThan21 = true;  //Boolean
+console.log('Age2 is more than 21:' + ageMoreThan21);
+
